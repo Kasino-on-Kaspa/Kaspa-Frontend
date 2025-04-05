556b6764d2db8be6cbb1e65ab6f4b4529fa3205e
Vishal <vismbs@pm.me>
2025-04-05 17:47:21 +0530

    feat: removed cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,12 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import path from "path";
-import { cloudflare } from "@cloudflare/vite-plugin";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
     tailwindcss(),
-    cloudflare(),
   ],
   resolve: {
     alias: {
