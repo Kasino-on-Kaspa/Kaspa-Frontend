6b86815bdb34a60545312b78c37f23c7e977344d
Vishal <vismbs@pm.me>
2025-04-12 19:23:30 +0530

    feat: added nix dev env files

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+    description = "Kaspa Kasino Frontend Flake";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    };
+
+    outputs = { self, nixpkgs, ... }: 
+    let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system; };
+    in
+    {
+        devShell.${system} = pkgs.mkShell {
+            buildInputs = with pkgs; [
+                nodejs
+                pnpm
+            ];
+        };
+    };
+}
